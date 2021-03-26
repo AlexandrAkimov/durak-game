cf57f6f31bfaf75fecba686db1a7a600b7a8f3e9
AlexandrAkimov <akimovy14072018@yandex.ru>
2021-03-26 15:27:50 +0300

    change min-height

--- src/App.svelte
+++ src/App.svelte
@@ -176,7 +176,7 @@
     flex-direction: column;
     justify-content: space-around;
     width: 100vw;
-    height: 100vh;
+    min-height: 100vh;
     background-image: url("/sukno.jpg");
     background-size: cover;
   }
